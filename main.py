a75db5683a580eb47157d1ff555e2d80462cbb95
Janquiel Kappler <41974499+jankyk@users.noreply.github.com>
2021-11-22 23:23:21 +0000

    iniciando

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+#importando os módulos
 import csv
-import randon
+import random
+
+#abrindo o arquivo de perguntas e respostas
+lista = open("PerguntasERespostas(utf8).csv","r")
+
+linha = lista.readline()
+print(linha)
+
+#fechando o arquivo de perguntas e respostas
+lista.close()
